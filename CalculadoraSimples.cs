8551ec68193e62bfaa34e471cc560c98642dd26f
eduardo2580 <edu.rodrigues2580@gmail.com>
2023-04-20 18:06:00 -0300

    Starting calculation code
    This commit adds part of the calculation code with known errors.

--- CalculadoraSimples.cs
+++ CalculadoraSimples.cs
@@ -45,24 +45,95 @@ namespace CalculadoraSimples
             txtDisplay.Text += (btn as Button).Text;
         }
 
-        private void btnPlus_Click(object sender, EventArgs e)
+        private void btnPlus_Click(object plus, EventArgs e)
         {
-
+            if (previousOperation == Operation.None)
+            {
+                previousOperation = Operation.Add;
+            }
+            else
+            {
+                performCalculation(previousOperation);
+            }
+            txtDisplay.Text += (plus as Button).Text;
         }
 
-        private void btnMinus_Click(object sender, EventArgs e)
+        private void performCalculation(Operation previousOperation)
         {
-
+            List<double> lstNums = new List<double>();
+            switch (previousOperation)
+            {
+                case Operation.Add:
+                    lstNums = txtDisplay.Text.Split('+').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] + lstNums[1]).ToString();
+                    break;
+                case Operation.Sub:
+                    lstNums = txtDisplay.Text.Split('-').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] - lstNums[1]).ToString();
+                    break;
+                case Operation.Mul:
+                    lstNums = txtDisplay.Text.Split('*').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] * lstNums[1]).ToString();
+                    break;
+                case Operation.Div:
+                    lstNums = txtDisplay.Text.Split('/').Select(double.Parse).ToList();
+                    txtDisplay.Text = (lstNums[0] / lstNums[1]).ToString();
+                    break;
+                case Operation.None:
+                    break;
+                default:
+                    break;
+            }
         }
 
-        private void btnMulti_Click(object sender, EventArgs e)
+            private void btnMinus_Click(object minus, EventArgs e)
         {
-
+            if (previousOperation == Operation.None)
+            {
+                previousOperation = Operation.Sub;
+            }
+            else
+            {
+                performCalculation(previousOperation);
+            }
+            txtDisplay.Text += (minus as Button).Text;
         }
 
-        private void btnDiv_Click(object sender, EventArgs e)
+        private void btnMulti_Click(object multi, EventArgs e)
         {
-
+            if (previousOperation == Operation.None)
+            {
+                previousOperation = Operation.Mul;
+            }
+            else
+            {
+                performCalculation(previousOperation);
+            }
+            txtDisplay.Text += (multi as Button).Text;
         }
+
+        private void btnDiv_Click(object div, EventArgs e)
+        {
+            if (previousOperation == Operation.None)
+            {
+                previousOperation = Operation.Div;
+            }
+            else
+            {
+                performCalculation(previousOperation);
+            }
+            txtDisplay.Text += (div as Button).Text;
+        }
+
+        enum Operation
+        {
+            Add,
+            Sub,
+            Mul,
+            Div,
+            None
+        }
+
+        static Operation previousOperation = Operation.None;
     }
 }
